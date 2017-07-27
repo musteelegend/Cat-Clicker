6f8b21cfb221357e4db6d9a04d673600e425e222
Abdul-Karim <mmkarim52@gmail.com>
2017-07-27 07:05:00 -0400

    Multiple cats Cat-Clicker

--- js/script.js
+++ js/script.js
@@ -1,16 +1,50 @@
+var cats = [
+                {
+                    clickCount : 0,
+                    name : 'Tabby',
+                    imageSrc : 'images/cat1.jpg',
+                    nicknames: ['Crazy Whiskers', 'One Eyes Cat', 'Cat WithManBalls']
+                },
+                {
+                    clickCount: 0,
+                    name : 'Teddy',
+                    imageSrc : 'images/cat2.jpg'
+                },
+                {
+                    clickCount : 0,
+                    name : 'John McCat',
+                    imageSrc : 'images/cat3.jpg'
+                },
+                {
+                    clickCount : 0,
+                    name : 'Sweetie',
+                    imageSrc : 'images/cat4.jpg'
+                },
+                {
+                    clickCount : 0,
+                    name : 'Black Beauty',
+                    imageSrc : 'images/cat5.jpg'
+                }
+            ];
+
 var ViewModel = function() {
     var self = this
-    this.currentCat = ko.observable( new Cat({
-        clickCount: 0,
-        name: 'Tabby',
-        imageSrc: 'images/cat1.jpg',
-        nicknames: ['Crazy Whiskers', 'One Eyes Cat', 'Cat WithManBalls']
 
-    }) );
+    this.catList = ko.observableArray([]);
+
+    cats.forEach(function(catItem){
+        self.catList.push(new Cat(catItem));
+    });
+
+    this.currentCat = ko.observable( this.catList()[0] );
 
     this.incrementCounter = function() {
         self.currentCat().clickCount(self.currentCat().clickCount() + 1);
     };
+
+    this.setCat = function(clickedCat) {
+        self.currentCat(clickedCat);
+    }
 };
 
 var Cat = function(data) {
