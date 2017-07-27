bf403b305e88c422e304b1b9fc2b31d67b30d70b
Abdul-Karim <mmkarim52@gmail.com>
2017-07-27 06:12:18 -0400

    moving data to change a multiple cat clicker

--- js/script.js
+++ js/script.js
@@ -1,10 +1,36 @@
 var ViewModel = function() {
-    this.clickCount = ko.observable(0);
-    this.name = ko.observable('Tabby');
-    this.imageSrc = ko.observable('images/cat1.jpg')
+    var self = this
+    this.currentCat = ko.observable( new Cat({
+        clickCount: 0,
+        name: 'Tabby',
+        imageSrc: 'images/cat1.jpg',
+        nicknames: ['Crazy Whiskers', 'One Eyes Cat', 'Cat WithManBalls']
+
+    }) );
 
     this.incrementCounter = function() {
-        this.clickCount(this.clickCount() + 1);
+        self.currentCat().clickCount(self.currentCat().clickCount() + 1);
     };
-}
+};
+
+var Cat = function(data) {
+     this.clickCount = ko.observable(data.clickCount);
+    this.name = ko.observable(data.name);
+    this.imageSrc = ko.observable(data.imageSrc);
+    this.nicknames = ko.observableArray(data.nicknames);
+
+
+    this.level = ko.computed(function() {
+        var title;
+        if(this.clickCount() < 10){
+            title = "infant";
+        } else if (this.clickCount() < 15){
+            title = "baby";
+        }else if(this.clickCount() < 20){
+            title = "teen";
+        }
+        return title;
+    }, this);
+
+};
 ko.applyBindings(new ViewModel());
